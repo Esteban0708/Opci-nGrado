4c36c02c9d7b3b151b5a39e6558f9638fea62944
Esteban0708 <ramosesteban682@gmail.com>
2025-02-10 22:08:45 -0500

    Incluyendo los cambios faltantes

--- src/app/view/ver-convocatorias/ver-convocatorias.component.ts
+++ src/app/view/ver-convocatorias/ver-convocatorias.component.ts
@@ -6,18 +6,63 @@ import { MenuService } from 'src/app/service/services/menu.service';
   templateUrl: './ver-convocatorias.component.html',
   styleUrls: ['./ver-convocatorias.component.css']
 })
-export class VerConvocatoriasComponent implements OnInit{
- isSidebarReduced: boolean = false;
- isModalOpen: boolean = false;
+export class VerConvocatoriasComponent implements OnInit {
+  isSidebarReduced: boolean = false;
+  isModalOpen: boolean = false;
+  mostrarOpciones: boolean = false;
+  filtroSeleccionado: string = 'Activo';
+  convocatorias = [
+    {
+      titulo: 'V CONVOCATORIA DE PROYECTOS DE EXTENSIÓN Y PROYECCIÓN SOCIAL',
+      fechaDesde: '09-08-2024',
+      fechaHasta: '09-11-2025',
+      estado: 'Activa'
+    },
+    {
+      titulo: 'VI CONVOCATORIA DE PROYECTOS DE EXTENSIÓN Y PROYECCIÓN SOCIAL',
+      fechaDesde: '09-08-2024',
+      fechaHasta: '09-11-2024',
+      estado: 'Inactiva'
+    },
+    {
+      titulo: 'VII CONVOCATORIA DE PROYECTOS DE EXTENSIÓN Y PROYECCIÓN SOCIAL',
+      fechaDesde: '09-08-2024',
+      fechaHasta: '09-11-2024',
+      estado: 'Inactiva'
+    },
+    {
+      titulo: 'VIII CONVOCATORIA DE PROYECTOS DE EXTENSIÓN Y PROYECCIÓN SOCIAL',
+      fechaDesde: '09-08-2024',
+      fechaHasta: '09-11-2024',
+      estado: 'Inactiva'
+    }
+  ];
 
   constructor(private menuService: MenuService) {}
-  
+
   ngOnInit() {
     this.menuService.isSidebarReduced$.subscribe((isReduced) => {
       this.isSidebarReduced = isReduced;
     });
   }
+
   Salir() {
     window.location.href = '/principal';
+  }
+
+  toggleFiltro() {
+    this.mostrarOpciones = !this.mostrarOpciones;
+  }
+
+  seleccionarFiltro(filtro: string) {
+    this.filtroSeleccionado = filtro;
+    this.mostrarOpciones = false;
+  }
+
+  get convocatoriasFiltradas() {
+    if (this.filtroSeleccionado === 'todos') {
+      return this.convocatorias;
+    }
+    return this.convocatorias.filter(c => c.estado === 'Activa');
   }
 }
